4cb7ccbd3c2d8054b8d9ee5e3a402aa9ff57434b
Gideon Adurota <gideonadurota@outlook.com>
2022-05-21 09:49:55 +0000

    Create subnet

--- main.tf
+++ main.tf
@@ -27,3 +27,12 @@ resource "azurerm_virtual_network" "V-Net" {
     location            = azurerm_resource_group.resourceGroup.location
     resource_group_name = azurerm_resource_group.resourceGroup.name
 }
+
+#Create subnets
+resource "azurerm_subnet" "AppDiscovery-subnet" {
+    count                = var.number-of-vms
+    name                 = "subnet-${count.index}"
+    resource_group_name  = azurerm_resource_group.resourceGroup.name
+    virtual_network_name = azurerm_virtual_network.V-Net.name
+    address_prefixes     = [element(var.subnet-address-space, count.index)]
+}
--- variable.tf
+++ variable.tf
@@ -16,3 +16,27 @@ variable "vnet" {
     default = "AppDisc-vnet"  
     type = string
 }
+
+variable "vnet-address-space" {
+    default = ["10.0.0.0/16"]
+}
+
+variable "number-of-vms" {
+  type = string
+}
+
+variable "subnet-address-space" {
+    type = list
+    default = [
+        "10.0.1.0/24",
+        "10.0.2.0/24",
+        "10.0.3.0/24",
+        "10.0.4.0/24",
+        "10.0.5.0/24",
+        "10.0.6.0/24",
+        "10.0.7.0/24",
+        "10.0.8.0/24",
+        "10.0.9.0/24",
+        "10.0.10.0/24"
+    ]
+}
