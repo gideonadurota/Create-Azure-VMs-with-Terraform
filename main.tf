d2c5070089e01bad0c9da45eb6973db6cac220d3
Gideon Adurota <gideonadurota@outlook.com>
2022-05-21 09:48:44 +0000

    Create virtual network

--- main.tf
+++ main.tf
@@ -19,3 +19,11 @@ resource "azurerm_resource_group" "resourceGroup" {
     name = var.resourceGroup
     location = var.location
 }
+
+#Create virtual network
+resource "azurerm_virtual_network" "V-Net" {
+    name                = var.vnet
+    address_space       = var.vnet-address-space
+    location            = azurerm_resource_group.resourceGroup.location
+    resource_group_name = azurerm_resource_group.resourceGroup.name
+}
--- variable.tf
+++ variable.tf
@@ -6,3 +6,13 @@ variable "resourceGroup" {
     default = "AppDiscover-rg"
     type = string
 }
+
+variable "location" {
+    default = "West Europe"
+    type = string
+}
+
+variable "vnet" {
+    default = "AppDisc-vnet"  
+    type = string
+}
