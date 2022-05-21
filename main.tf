44a81becebf5b2a422c6e34e272637237cb92b54
Gideon Adurota <gideonadurota@outlook.com>
2022-05-21 09:50:42 +0000

    Create network interface

--- main.tf
+++ main.tf
@@ -36,3 +36,22 @@ resource "azurerm_subnet" "AppDiscovery-subnet" {
     virtual_network_name = azurerm_virtual_network.V-Net.name
     address_prefixes     = [element(var.subnet-address-space, count.index)]
 }
+
+#Create network interface
+resource "azurerm_network_interface" "vmnic" {
+  # for_each                      = azurerm_subnet.AppDiscovery-subnet
+  # name                 = "vm-${length(var.subnet-address-space).index}"
+  count               = var.number-of-vms
+  name                = "vm-${count.index}-nic"
+  location            = azurerm_resource_group.resourceGroup.location
+  resource_group_name = azurerm_resource_group.resourceGroup.name
+
+  ip_configuration {
+    #for_each                      = azurerm_subnet.AppDiscovery-subnet
+    #name                          = "testconfiguration-${count.index}"
+    name                          = "testconfiguration-${count.index}"
+    subnet_id                     = azurerm_subnet.AppDiscovery-subnet[count.index].id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public-ip-address[count.index].id
+  }
+}
